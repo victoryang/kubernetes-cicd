7a2768de4177c6eec8cf340338500a71ba78a434
victoryang <yangtao3601@126.com>
2020-12-29 00:19:12 +0800

    update gorm

--- go.mod
+++ go.mod
@@ -4,15 +4,23 @@ go 1.14
 
 require (
 	github.com/elazarl/go-bindata-assetfs v1.0.1
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/jinzhu/gorm v1.9.16
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.3 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.1.1
-	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07
+	github.com/tebeka/strftime v0.1.5 // indirect
 	github.com/xanzy/go-gitlab v0.40.2
 	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
 	gopkg.in/ldap.v2 v2.5.1
 	gopkg.in/yaml.v2 v2.3.0
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.9
 	k8s.io/api v0.19.4
 	k8s.io/apimachinery v0.19.4
 	k8s.io/client-go v0.19.0
--- orm/mysql.go
+++ orm/mysql.go
@@ -2,9 +2,10 @@ package orm
 
 import (
 	"fmt"
-	
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql" // mysql driver
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"gorm.io/driver/mysql"
 
 	"github.com/victoryang/kubernetes-cicd/config"
 )
@@ -14,14 +15,16 @@ var MySQL *gorm.DB
 
 // TODO: add mysql table index
 func InitMysqlModule(c *config.DatabaseConfig) {
-	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return "sce_" + defaultTableName
-	}
-
-	connStr := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/rolling?parseTime=true&charset=utf8&loc=Local", c.Username, c.Password)
 	var err error
-	fmt.Println(connStr)
-	MySQL, err = gorm.Open("mysql", connStr)
+	dsn := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/rolling?parseTime=true&charset=utf8&loc=Local", c.Username, c.Password)
+
+	config := &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: "sce_",
+			SingularTable: false,
+		},
+	}
+	MySQL, err = gorm.Open(mysql.Open(dsn), config)
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect database: %s", err))
 	}
